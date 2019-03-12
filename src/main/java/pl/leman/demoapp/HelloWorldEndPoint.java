fce27141600d38668623fafb5c1202bcaa9efda7
Bartek <barteklemanski@o2.pl>
2019-03-12 09:18:45 +0100

    init repo

---
+++ src/main/java/pl/leman/demoapp/HelloWorldEndPoint.java
@@ -0,0 +1,15 @@
+package pl.leman.demoapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldEndPoint {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    String hello(){
+        return "Hello World!";
+    }
+
+}
